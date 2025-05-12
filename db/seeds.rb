6c8d2a1fde00e1e5d369f82a81dfcf18169fa0f6
Harriet Anderson <harriet.correspondence@gmail.com>
2025-05-12 12:47:36 +0100

    seeded with 4 recipes, no urls for images yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Recipe.create(name: "Overnight Porridge", description: "Easy recipe for luxury whole-food breakfast", rating: 5.0)
+Recipe.create(name: "Devilled Bacon", description: "Simple, tasty cheap 'student food' ", rating: 4.7)
+Recipe.create(name: "Basic Omelette", description: "Two ingredient meal, ", rating: 4.5)
+Recipe.create(name: "Roast Halloumi and Sweet Potatoes", description: "Very quick dinner,  roast halloumi with sweet potatoes", rating: 3.4)
