65411e1591c472eb3dab0f71730b8d60603d28db
Harriet Anderson <harriet.correspondence@gmail.com>
2025-05-12 15:38:47 +0100

    create Category is functioning, I will add navigation in step 9

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,7 +9,26 @@ class CategoriesController < ApplicationController
   end
 
   def new
-    @recipe = Recipe.new
+    @category = Category.new
   end
 
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to category_path(@category)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  # def set_list
+  #   @list = List.find(params[:id])
+  # end
+
+   def category_params
+     params.require(:category).permit(:name)
+   end
+  # taken from watchlist solution
 end
