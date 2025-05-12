b82b4fa6f7d5e73918699c3f18a94711c38fe5e8
Harriet Anderson <harriet.correspondence@gmail.com>
2025-05-12 11:27:33 +0100

    validations complete but not yet tested

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :recipe
   belongs_to :category
+  validates :comment, length: { minimum: 6 }
+  validates_uniqueness_of :recipe_id, :scope => [:cateogory_id]
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,6 @@
 class Recipe < ApplicationRecord
   has_many :bookmarks
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :rating, numericality: { in: (0..5) }
 end
